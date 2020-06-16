c2826e515a3bba46a146d937c3bff2ec6c9ed814
rumenkrumov3 <64036794+rumenkrumov3@users.noreply.github.com>
2020-06-16 21:32:14 +0300

    Commit
    Commit

---
+++ SeleniumAdvance/Factories/AutomationPracticeRegistrationFactory.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture;
+using SeleniumAdvance.Pages.AutomationPracticeRegistration.AutomationPracticeModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumAdvance.Pages.AutomationPracticeRegistration.AutomationPracticeFactory
+{
+    public static class AutomationPracticeRegistrationFactory
+    {
+        public static AutomationPracticeRegistrationModel Create()
+        {
+            var fixture = new Fixture();
+            var email = fixture.Create<string>();
+
+            return new AutomationPracticeRegistrationModel
+            {
+                FirstName = "Rumen",
+                LastName = "Krumov",
+                Password = "Q1w2e3r4t5y6",
+                Address = "asgasgasgasgasgas",
+                City = "Sofia",
+                State = "Alaska",
+                PostCode = "17000",
+                MobilePhone = "125125125125",
+                AssingAdress = "ahashashashash",
+                Email = $"{email}@abv.bg"
+            };
+        }
+    }
+}
---
+++ SeleniumAdvance/Models/AutomationPracticeRegistrationModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumAdvance.Pages.AutomationPracticeRegistration.AutomationPracticeModel
+{
+    public class AutomationPracticeRegistrationModel
+    {
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Days { get; set; }
+        public string Months { get; set; }
+        public string Year { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostCode { get; set; }
+        public string MobilePhone { get; set; }
+        public string AssingAdress { get; set; }
+
+    }
+}
